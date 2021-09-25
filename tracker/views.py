3bce825bd3cda1de70b1b74403335acf06d594c8
Timothy Madegwa <timothymadegwa@gmail.com>
2021-09-25 20:11:28 +0300

    added ui for events and report page

--- tracker/views.py
+++ tracker/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'tracker/index.html')
+
+def report(request):
+    return render(request, 'tracker/report.html')
